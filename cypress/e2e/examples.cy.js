9096b61907bf8209f559290c724b8da8d60eabf0
Tegacreatives <contact@tegacreatives.com>
2023-09-18 23:58:53 +0100

    multi page testing

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,3 +1,27 @@
 describe("Various examples", () => {
-  it("multi-page examples", () => {});
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("multi-page examples", () => {
+    cy.getDataTest("nav-why-cypress").click();
+    cy.location("pathname").should("equal", "/");
+
+    cy.getDataTest("nav-overview").click();
+    cy.location("pathname").should("equal", "/overview");
+
+    cy.getDataTest("nav-examples").click();
+    cy.location("pathname").should("equal", "/examples");
+
+    cy.getDataTest("nav-fundamentals").click();
+    cy.location("pathname").should("equal", "/fundamentals");
+
+    cy.getDataTest("nav-component").click();
+    cy.location("pathname").should("equal", "/component");
+
+    cy.getDataTest("nav-forms").click();
+    cy.location("pathname").should("equal", "/forms");
+
+    cy.getDataTest("nav-best-practices").click();
+    cy.location("pathname").should("equal", "/best-practices");
+  });
 });
